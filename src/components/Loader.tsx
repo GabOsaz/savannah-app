c95d471a1bb430985ecede3233db856f15b8cc84
Omo Osaze <okpebhoo@gmail.com>
2025-07-24 11:30:40 +0100

    feat: Use recommended loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,7 +3,8 @@ const Loader = ({ message }: { message: string }) => {
     <div className="max-w-4xl mx-auto">
       <div className="flex items-center justify-center min-h-[400px]">
         <div className="text-center">
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900 mx-auto mb-4" />
+        <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+          {/* <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900 mx-auto mb-4" /> */}
           <p className="text-gray-600">{message}</p>
         </div>
       </div>
